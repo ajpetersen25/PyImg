653671801ce7fc1b9a36257c9a461f59e4d5f6dc
ajpetersen25 <whaticannotcreate@gmail.com>
2022-12-15 08:04:03 -0800

    Create backsub_imgs.py

---
+++ pyimg/backsub_imgs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import background_subtraction as bs
+import numpy as np
+import os,sys
+import argparse
+import copy
+import getpass
+from datetime import datetime
+
+def main():
+  ''' Background subtract images from the command line. '''
+  
+  
+  
+if __name__ == "__main__":
+  main()
