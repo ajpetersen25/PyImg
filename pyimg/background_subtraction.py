398a7ee4c195655ec3462743896c4971103d5913
ajpetersen25 <whaticannotcreate@gmail.com>
2022-12-15 19:59:54 -0800

    Update background_subtraction.py

--- pyimg/background_subtraction.py
+++ pyimg/background_subtraction.py
@@ -42,7 +42,7 @@ def backsub_imgs(img_dir, save_dir, method,f_increment=1,paired=False,n_frames='
   # if save_dir doesn't exist, create it
   # imwrite in save_dir
   
-def moving_min(imgs,paired=False,f_increment):
+def moving_min(imgs,paired=False,f_increment=1):
   " input list of images "
   backsubed = list()
   if paired==False:
@@ -52,7 +52,7 @@ def moving_min(imgs,paired=False,f_increment):
     frames = np.arange(0,len(imgs),f_increment)
     width = 100
     for fi, f in enumerate(frames):
-      d = imgs[0].max*np.ones(img[0].shape)
+      d = imgs[0].max*np.ones(imgs[0].shape)
       if fi<width:
         for i in np.arange(0,(2*width)):
           d = np.minimum(d,imgs[i])
@@ -73,8 +73,8 @@ def moving_min(imgs,paired=False,f_increment):
     print('Finding minimum')
     width = 100
     for fi in np.arange(0,len(imgs),2):
-      d1 = imgs[0].max*np.ones(img[0].shape)
-      d2 = imgs[0].max*np.ones(img[0].shape)
+      d1 = imgs[0].max*np.ones(imgs[0].shape)
+      d2 = imgs[0].max*np.ones(imgs[0].shape)
       if fi<width:
         for i in np.arange(0,(2*width),2):
           d1 = np.minimum(d1,imgs[i])
@@ -103,7 +103,7 @@ def min_sub(imgs,paired=False,f_increment=1):
   if paired==False:
     print('Continuous background subtraction')
     print('Finding minimum')
-    d = imgs[0].max*np.ones(img[0].shape)
+    d = imgs[0].max*np.ones(imgs[0].shape)
     frames = np.arange(0,len(imgs),f_increment)
     for f in frames:
       d = np.minimum(d,f)
@@ -116,8 +116,8 @@ def min_sub(imgs,paired=False,f_increment=1):
   elif paired==True:
     print('Paired background subtraction')
     print('Finding minimum')
-    d1 = imgs[0].max*np.ones(img[0].shape)
-    d2 = imgs[1].max*np.ones(img[1].shape)
+    d1 = imgs[0].max*np.ones(imgs[0].shape)
+    d2 = imgs[1].max*np.ones(imgs[1].shape)
     for f in np.arange(0,len(imgs),2):
       d1 = np.minimum(d1,imgs[f])
       d2 = np.minimum(d2,imgs[f+1])
@@ -129,37 +129,39 @@ def min_sub(imgs,paired=False,f_increment=1):
    
   return(backsubed)
   
-def mean_sub(imgs,paired=False,f_increment=1):
+def mean_sub(img_files,paired=False,f_increment=1):
   " input list of images "
   if paired==False:
     print('Continuous background subtraction')
     print('Finding minimum')
-    d = np.zeros(img[0].shape)
-    frames = np.arange(0,len(imgs),f_increment)
+    d = np.zeros(iio.imread(img_files[0]).shape)
+    frames = np.arange(0,len(img_files),f_increment)
     for f in frames:
-      d += f
+      d += iio.imread(img_files[f])
     d = d/len(frames)
     print('Performing backgroud subtraction\n')
     backsubed = list()
-    for img in imgs:
+    for i in img_files:
+      img = iio.imread(i)
       backsubed.append(img - d)
 
-      
+
   elif paired==True:
     print('Paired background subtraction')
     print('Finding minimum')
-    d1 = np.zeros(img[0].shape)
-    d2 = np.zeros(img[1].shape)
+    d1 = np.zeros(iio.imread(img_files[0]).shape)
+    d2 = np.zeros(iio.imread(img_files[1]).shape)
     for f in np.arange(0,len(imgs),2):
-      d1 += imgs[f]
-      d2 += imgs[f+1]
+      d1 += iio.imread(img_files[f])
+      d2 += iio.imread(img_files[f+1])
     d1 = d1/(len(imgs)/2)
+    d2 = d2/(len(imgs)/2)
     print('Performing backgroud subtraction\n')
     backsubed = list()
     for f in np.arange(0,len(imgs),2):
-      backsubed.append(imgs[f] - d1)
-      backsubed.append(imgs[f+1] - d2)
-   
+      backsubed.append(iio.imread(img_files[f]) - d1)
+      backsubed.append(iio.imread(imgs[f+1]) - d2)
+
   return(backsubed)
   
   
